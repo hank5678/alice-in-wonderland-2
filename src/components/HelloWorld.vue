fba980a79f3143d9d8583e01193d066d39319712
阿貴 <hank5678tw@gmail.com>
2022-12-24 22:19:28 +0800

    feat: init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>Hello World~</p>
+  <p>叉叉: ⓧ</p>
+  <p>勾勾: ✔</p>
+  <span>LOGO: <img :src="logo" alt="" /></span>
+</template>
+
+<script setup>
+  import logo from "../assets/logo.svg"
+</script>
+
+<style scoped>
+  p {
+    font-size: 24px;
+  }
+  img {
+    width: 48px;
+  }
+</style>
